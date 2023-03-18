e87f0624c76a93bc1cce299dc811ef3b8218cd86
cory hain <sullenchimp@gmail.com>
2023-03-17 19:51:29 -0700

    added projects

--- Data/Class1.cs
+++ Data/Class1.cs
@@ -4,4 +4,5 @@
     {
 
     }
+    //Server=tcp:funcogtyp-sqlsrv.database.windows.net,1433;Initial Catalog=funcogtyp_sqldb;Persist Security Info=False;User ID=funcogtype-admin;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
 }
